357d89017069f6ba7acf4b71947cac5b1f28513f
Joseph Maiocco <33736943+jmaiocco@users.noreply.github.com>
2018-01-14 21:21:35 -0800

    Add initial file header

--- homework00/Greeter.java
+++ homework00/Greeter.java
@@ -1,3 +1,15 @@
+/** 
+ *  File name     :  Greeter.java
+ *  Purpose       :  first Java program; says "Hello, world"
+ *  @author       :  Joe Maiocco
+ *  Date          :  2018-01-11
+ *  Description   :  @See purpose
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  @exception   :  None
+ *  @version      :  1.0.0
+ **/
+
 public class Greeter {
 	public static void main( String[] args ) {
 		System.out.println( "Hello, world!");
