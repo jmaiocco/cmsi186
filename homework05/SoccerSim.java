76fe71e1b782a334560152a8ca85f5406487002a
Joseph Maiocco <33736943+jmaiocco@users.noreply.github.com>
2018-03-15 11:01:01 -0700

    Update SoccerSim.java

--- homework05/SoccerSim.java
+++ homework05/SoccerSim.java
@@ -1,1 +1,38 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Ball.java
+ *  Purpose       :  Provides a class defining methods for the SoccerSim 
+ *  @author       :  Joe Maiocco
+ *  Date written  :  2018-03-13
+ *  Description   :  
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  IllegalArgumentException when the input arguments are unacceptable 
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
+public class SoccerSim {
+
+  /**
+   *  Class field definitions go here
+   */
+  public static final double FIELD_SIZE = 400.0;
+  public static final double FRICTIONAL_PERCENTAGE = 0.01;
+  public Ball[] ballCollection;
+
+  public SoccerSim() {
+  	super();
+  }
+
+  public boolean detectCollision( Ball b1, Ball b2 ) {
+  	if ( b1.getXPosition() == b2.getXPosition() && b1.getYPosition() == b2.getYPosition() ) {
+  	  return true;	
+  	} else {
+  	  return false;
+  	}
+  }
+
+  public static void main( String args ) {
+  	Clock clock = new Clock();
+  	Ball[] ballCollection = Ball[ ( args.length - 1 ) % 4 ];
+
+  }
+}
