87be6d5dfee348339406af90bb8c8fa217502033
Joseph Maiocco <33736943+jmaiocco@users.noreply.github.com>
2018-03-15 15:57:32 -0700

    Update method descriptions

--- homework05/Ball.java
+++ homework05/Ball.java
@@ -16,14 +16,14 @@ public class Ball {
    */
    private static final double FIELD_SIZE = 400.0;
    private static final double FRICTIONAL_PERCENTAGE = 0.99;
-   private static final double BALL_RADIUS = 4.45;
-   private double ballXPosition, ballYPosition, ballXSpeed, ballYSpeed, modifiedBallSpeed;
+   private static final double BALL_RADIUS = 0.308734;
+   private double ballRadius, ballXPosition, ballYPosition, ballXSpeed, ballYSpeed, modifiedBallSpeed;
 
   /**
    *  Constructor
    */
    public Ball( double xPosition, double yPosition, double xSpeed, double ySpeed ) {
-   	 if ( ( xPosition > FIELD_SIZE/2 ) || ( xPosition < -FIELD_SIZE/2 ) || ( yPosition > FIELD_SIZE/2 ) || ( yPosition < -FIELD_SIZE/2 ) ) ) {
+   	 if ( ( ( xPosition > FIELD_SIZE/2 ) || ( xPosition < -FIELD_SIZE/2 ) || ( yPosition > FIELD_SIZE/2 ) || ( yPosition < -FIELD_SIZE/2 ) ) ) {
    	   throw new IllegalArgumentException();
    	 }
    	 this.ballRadius    = BALL_RADIUS;
@@ -34,7 +34,8 @@ public class Ball {
     } 
 
   /**
-   *
+   *  Method to update a ball's horizontal position
+   *  @return double-precision value of the ball's new x-position 
    */
     public double moveBallHorizontally() {
       ballXPosition += ballXSpeed;
@@ -42,7 +43,8 @@ public class Ball {
     }
 
   /**
-   *
+   *  Method to update a ball's vertical position
+   *  @return double-precision value of the ball's new y-position 
    */
     public double moveBallVertically() {
       ballYPosition += ballYSpeed;
@@ -50,22 +52,26 @@ public class Ball {
     }
 
   /**
-   *
+   *  Method to reduce a ball's velocity using 
+   *  @param  givenBallSpeed  double-precision value of the ball's original speed component  
+   *  @return double-precision value of the ball's new speed component
    */
     public double applyFriction( double givenBallSpeed ) {
-      modifiedBallSpeed = ( givenBallSpeed * FRICTION_COEFFICIENT ) ///*timeslice;
+      modifiedBallSpeed = ( givenBallSpeed * FRICTIONAL_PERCENTAGE ); ///*timeslice;
       return modifiedBallSpeed;
     }
 
   /**
-   *
+   *  Method to retrieve a ball's current x-position
+   *  @return double-precision value of the ball's current x-position
    */
     public double getXPosition() {
       return ballXPosition;
     }
 
   /**
-   *
+   *  Method to retrieve a ball's current y-position
+   *  @return double-precision value of the ball's current y-position
    */
     public double getYPosition() {
       return ballYPosition;
@@ -73,7 +79,7 @@ public class Ball {
 
   /**
    * Public Instance method that returns a String representation of THIS ball instance
-   * @return String representation of this Die
+   * @return String representation of this Ball
    */
    public String toString() {
     return "Position: (" + ballXPosition + "," + ballYPosition + ") and Velocity: <" + ballXSpeed + "," + ballYSpeed + ">" ;
@@ -88,10 +94,22 @@ public class Ball {
    }
 
   /**
-   *
+   *  Main program
    */
    public static void main( String args[] ) {
-   	Ball b1 = new Ball();
-   } 
+     try { Ball b1 = new Ball( 0, 0, 0, 0 ); }
+   	 catch( IllegalArgumentException iae ) { System.out.println( "Invalid starting position of (0,0)" ); }
+   	 try { Ball b1 = new Ball( 250, 250, -1, 1 ); }
+   	 catch( IllegalArgumentException iae ) { System.out.println( "Invalid starting position of (250,250)" ); }
+   	 try { Ball b1 = new Ball( -250, -249, -2000, 2000 ); }
+   	 catch( IllegalArgumentException iae ) { System.out.println( "Invalid starting position of (-250,-249)" ); }
+   	 try { Ball b1 = new Ball( 251, 0, 1.7923, -3 ); }
+   	 catch( IllegalArgumentException iae ) { System.out.println( "Invalid starting position of (251,0)" ); }
+   	 try { Ball b2 = new Ball( 74, -148, -4, 2.75 ); }
+   	 catch( IllegalArgumentException iae ) { System.out.println( "Invalid starting position of (74,-148)"); }
+   	 Ball b2 = new Ball( 74, -148, -4, 2.75 );
+   	 b2.toString();
+    }
+
 
 }
